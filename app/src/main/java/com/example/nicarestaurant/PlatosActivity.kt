448a28a20067c505deaeaf2e3ea6a2ea168c849a
abcmrsty <renatasofiacastillo60@gmail.com>
2024-08-25 09:19:57 -0600

    First commit

---
+++ app/src/main/java/com/example/nicarestaurant/PlatosActivity.kt
@@ -0,0 +1,20 @@
+package com.example.nicarestaurant
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PlatosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_platos)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
